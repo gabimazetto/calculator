7b8f91e9a0a3c1cbb5b7ffee1a869101a2d8a796
gabimazetto <gmazettog@gmail.com>
2023-06-12 17:07:48 -0300

    Operações principais funcionando

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -4,7 +4,10 @@ import './Calculator.css';
 
 export function Calculator() {
 
-    const [num, setNum] = useState('');
+    const [num, setNum] = useState(0);
+    const [oldnum, setOldNum] = useState(0);
+    const [operator, setOperator] = useState();
+
 
     function inputNum(e) {
         const input = e.target.id;
@@ -15,9 +18,41 @@ export function Calculator() {
         }
     }
 
-    function clear(e) {
+    function clear() {
         setNum(0);
     }
+
+    function porcentage() {
+        setNum(num / 100);
+    }
+
+    function changeSign() {
+        if(num > 0){
+            setNum(-num);
+        }else {
+            setNum(Math.abs(num));
+        }
+    }
+
+    function calculate() {
+        if(operator === "/"){
+            setNum(parseFloat(oldnum) / parseFloat(num));
+        } else if(operator === "*"){
+            setNum(parseFloat(oldnum) * parseFloat(num));
+        } else if(operator === "+"){
+            setNum(parseFloat(oldnum) + parseFloat(num));
+        }else if(operator === "-"){
+            setNum(parseFloat(oldnum)- parseFloat(num));
+        }
+    }
+
+    function operatorHandle(e) {
+        const operatorInput = e.target.id;
+        setOperator(operatorInput);
+        setOldNum(num);
+        setNum(0);
+    }
+
 
     return (
         <>
@@ -34,32 +69,32 @@ export function Calculator() {
                         <table>
                             <tr>
                                 <td><button className="btn-operator" id="clear" onClick={clear}>C</button></td>
-                                <td><button className="btn-operator" id="/">&divide;</button></td>
-                                <td><button className="btn-operator" id="*">&times;</button></td>
-                                <td><button className="btn-operator" id="backspace">&lt;</button></td>
+                                <td><button className="btn-operator" id="/" onClick={operatorHandle}>&divide;</button></td>
+                                <td><button className="btn-operator" id="*" onClick={operatorHandle}>&times;</button></td>
+                                <td><button className="btn-operator" id="backspace" onClick={porcentage}>%</button></td>
                             </tr>
                             <tr>
                                 <td><button className="btn-number" id="7" onClick={inputNum}>7</button></td>
                                 <td><button className="btn-number" id="8" onClick={inputNum}>8</button></td>
                                 <td><button className="btn-number" id="9" onClick={inputNum}>9</button></td>
-                                <td><button className="btn-operator" id="-">-</button></td>
+                                <td><button className="btn-operator" id="-" onClick={operatorHandle}>-</button></td>
                             </tr>
                             <tr>
                                 <td><button className="btn-number" id="4" onClick={inputNum}>4</button></td>
                                 <td><button className="btn-number" id="5" onClick={inputNum}>5</button></td>
                                 <td><button className="btn-number" id="6" onClick={inputNum}>6</button></td>
-                                <td><button className="btn-operator" id="+" >+</button></td>
+                                <td><button className="btn-operator" id="+" onClick={operatorHandle}>+</button></td>
                             </tr>
                             <tr>
                                 <td><button className="btn-number" id="1" onClick={inputNum}>1</button></td>
                                 <td><button className="btn-number" id="2" onClick={inputNum}>2</button></td>
                                 <td><button className="btn-number" id="3" onClick={inputNum}>3</button></td>
-                                <td rowSpan="2"><button className="btn-equal" id="equal">=</button></td>
+                                <td rowSpan="2"><button className="btn-equal" id="equal" onClick={calculate}>=</button></td>
                             </tr>
                             <tr>
-                                <td><button className="btn-operator" id="(">(</button></td>
+                                <td><button className="btn-operator" id="+/-" onClick={changeSign}>+/-</button></td>
                                 <td><button className="btn-number" id="0" onClick={inputNum}>0</button></td>
-                                <td><button className="btn-operator" id=")">)</button></td>
+                                <td><button className="btn-operator" id="." onClick={inputNum}>.</button></td>
 
                             </tr>
                         </table>
